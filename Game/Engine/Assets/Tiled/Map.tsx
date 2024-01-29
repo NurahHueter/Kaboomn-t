10ed8133aa64bb5c6bae7b4a3f89da3f0a8c6384
Nurah Hüter <nhueter.mmt-b2022@fh-salzburg.ac.at>
2024-01-29 11:46:13 +0100

    fix: changed the tile type, map can be loaded now

--- Game/Engine/Assets/Tiled/Map.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Map" tilewidth="16" tileheight="16" tilecount="77" columns="11">
- <image source="../Sprout Lands - Sprites - premium pack/Tilesets/ground tiles/New tiles/Darker_Grass_Hills_Tiles_v2.png" width="176" height="112"/>
-</tileset>
